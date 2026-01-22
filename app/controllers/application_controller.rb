45f5e27f623a5aa888b02cd34dc15f9fcce801d1
Lovro Zelić <lovro.zelic@decode.agency>
2026-01-22 15:04:52 +0100

    add default locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_locale
+  before_action :authenticate_user!
+
+  protected
+
+  def admin_required
+    redirect_to root_path unless current_user&.admin?
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 end
